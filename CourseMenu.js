69abb6774527e8e3d79b596cc102e102f50250ed
Wahid-Haidari <wahidullahhaidari2@gmail.com>
2022-04-28 19:49:45 -0500

    Added new CanvasMenu, CourseMenu and submit.html

---
+++ CourseMenu.js
@@ -0,0 +1,38 @@
+class CourseMenu extends HTMLElement {
+	constructor(){
+		super();
+	}
+
+	connectedCallback(){
+  	this.innerHTML = `
+  	<style>
+  		#course-menu{
+			padding: 1em;
+			color:#993333 ;
+		}
+
+		#assignmentOption{
+			color: black;
+		}
+  	</style>
+  	<div id="course-menu">
+		<p>Home</p>
+		<p>Announcements</p>
+		<p id="assignmentOption">Assignments</p>
+		<p>Discussions</p>
+		<p>Grades</p>
+		<p>People</p>
+		<p>Pages</p>
+		<p>Files</p>
+		<p>Syllabus</p>
+		<p>BigBlueButton</p>
+		<p>Collaborations</p>
+		<p>New Analytics</p>
+		<p>TopHat LTI 1.3</p>
+		<p>Scanner</p>
+	</div>
+  	`;
+  }
+}
+
+customElements.define('course-menu' , CourseMenu);
